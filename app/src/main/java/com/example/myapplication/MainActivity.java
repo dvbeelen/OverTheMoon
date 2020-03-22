107cb18eae4e1ce938beb0d2abe2b6913920739f
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2020-03-22 21:26:02 +0100

    Connected Settings to MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
+    //Set variables
+    public Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set button onClickListeners
+        settings = findViewById(R.id.settings);
+        settings.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.settings:
+                Intent settingsScreen = new Intent(this, SettingsActivity.class);
+                startActivity(settingsScreen);
+                break;
+        }
+    }
+
 }
