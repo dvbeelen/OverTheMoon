8bbe92b0c47f9876cbc23d176ac77c9a0f5c6e86
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2020-03-30 20:11:01 +0200

    Added Titlescreen

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,38 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener{
 
-    //Set variables
-    public Button settings;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Set button onClickListeners
-        settings = findViewById(R.id.settings);
-        settings.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View view)
-    {
-        switch (view.getId()) {
-            case R.id.settings:
-                Intent settingsScreen = new Intent(this, SettingsActivity.class);
-                startActivity(settingsScreen);
-                break;
-        }
     }
-
 }
---
+++ app/src/main/java/com/example/myapplication/TitleActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TitleActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    public Button startButton;
+    public Button settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+
+        //Set button onClickListeners
+        settings = findViewById(R.id.settings);
+        settings.setOnClickListener(this);
+        startButton = findViewById(R.id.start);
+        startButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.start:
+                Intent mainActivity = new Intent(this, MainActivity.class);
+                startActivity(mainActivity);
+                break;
+            case R.id.settings:
+                Intent settingsScreen = new Intent(this, SettingsActivity.class);
+                startActivity(settingsScreen);
+                break;
+        }
+    }
+
+}
